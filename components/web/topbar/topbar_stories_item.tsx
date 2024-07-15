dce433122f8defaf532ce4c1d1d7b7bed7f7dc6c
aakarshshrivastav <168319817+aakarshshrivastav@users.noreply.github.com>
2024-07-15 23:24:11 +0530

    Update topbar_stories_item.tsx

--- components/web/topbar/topbar_stories_item.tsx
+++ components/web/topbar/topbar_stories_item.tsx
@@ -8,7 +8,7 @@ const TopbarStoriesItem = ({storyNumber, isExpanded,  handleOpenStory, src , tit
     <DialogTrigger onClick={()=>handleOpenStory(storyNumber)}>
 
     <div className={` flex flex-col ${!isExpanded ? "lg:mx-3" : "lg:mx-1 "} mx-3 justify-center items-center hover:bg-quarternary hover:scale-110 transition-all rounded-2xl  `}>
-    <div className="rounded-full   p-1 w-16 h-16 bg-[#80A385]">
+    <div className="rounded-full   p-1 w-16 h-16 bg-[#DAD1BF]">
       <div className=" bg-white rounded-full  h-full w-full   ">
 
         <img src={src!="" ? src: "https://media.assettype.com/sentinelassam-english%2F2024-06%2Fbd7021c0-5cc6-4de0-a725-d01b905f4b0c%2Fwomen.png?w=1200&ar=40%3A21&auto=format%2Ccompress&ogImage=true&mode=crop&enlarge=true&overlay=false&overlay_position=bottom&overlay_width=100"} className='h-full w-full rounded-full  bg-contain ' alt=""/>
